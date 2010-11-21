1e8f7c54fd466ff589b4365b59727916732c58fb
Vinai Kopp <vinai@netzarbeiter.com>
2010-11-21 20:14:34 +0100

    Fix NicerImageNames for use with pre 1.4.1 versions

--- Model/Observer.php
+++ Model/Observer.php
@@ -9,7 +9,7 @@ class Netzarbeiter_NicerImageNames_Model_Observer
 	 */
 	public function controllerFrontInitBefore($observer)
 	{
-		if (version_compare(Mage::getVersion(), '1.4.1', '<') && strpos(Mage::getVersion(), '-devel') === 'false')
+		if (version_compare(Mage::getVersion(), '1.4.1', '<') && strpos(Mage::getVersion(), '-devel') === false)
 		{
 			Mage::getConfig()->setNode('global/models/catalog/rewrite/product_image', 'Netzarbeiter_NicerImageNames_Model_Pre141_Image');
 		}
